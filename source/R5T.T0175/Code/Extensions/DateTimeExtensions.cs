eaf1d1744fde9ce033e6c0542b0aca993be89234
davidcoats <david@getsafetycone.com>
2023-04-28 16:56:43 -0700

    Interim changes.

--- source/R5T.T0175/Code/Extensions/DateTimeExtensions.cs
+++ source/R5T.T0175/Code/Extensions/DateTimeExtensions.cs
@@ -5,7 +5,7 @@ namespace R5T.T0175.Extensions
 {
     public static class DateTimeExtensions
     {
-        public static Timestamp ToTimestamp(this DateTime value)
+        public static ITimestamp ToTimestamp(this DateTime value)
         {
             return Instances.DateTimeOperator.ToTimestamp(value);
         }
--- source/R5T.T0175/Code/Functionality/IDateTimeOperator.cs
+++ source/R5T.T0175/Code/Functionality/IDateTimeOperator.cs
@@ -8,7 +8,7 @@ namespace R5T.T0175
     [FunctionalityMarker]
     public partial interface IDateTimeOperator : IFunctionalityMarker
     {
-        public Timestamp ToTimestamp(DateTime value)
+        public ITimestamp ToTimestamp(DateTime value)
         {
             var output = new Timestamp(value);
             return output;
